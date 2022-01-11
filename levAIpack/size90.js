5eb960556b58a1107e842965f798408cede2f9ab
kissjgabi <kissjgabi@gmail.com>
2022-01-11 13:59:10 +0100

    Update size90.js

--- levAIpack/size90.js
+++ levAIpack/size90.js
@@ -1,4 +1,1 @@
-Vákuumtasak 200*400*85 39Ft/db
-Vákuumtasak 250*450*85 55Ft/db
-Vákuumtasak 300*600*85 87Ft/db
-Vákuumtasak 300*600*85 79Ft/db
+const size90 = ["Válassz!", "Vákuumtasak 200*400*85 39Ft/db", "Vákuumtasak 250*450*85 55Ft/db", "Vákuumtasak 300*600*85 87Ft/db", "Vákuumtasak 300*600*85 79Ft/db"];
