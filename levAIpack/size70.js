64e0b9cc9890fb0b29a9d96dcd8d59fb2af638a5
kissjgabi <kissjgabi@gmail.com>
2022-01-13 12:59:57 +0100

    Update size70.js

--- levAIpack/size70.js
+++ levAIpack/size70.js
@@ -1,7 +1,7 @@
 const size70 = [
-    "24000 Vákuumtasak 150*200 12Ft/db",
-    "19000 Vákuumtasak 150*400 24Ft/db",
-    "30000 Vákuumtasak 150*500 30Ft/db",
-    "18000 Vákuumtasak 160*200 13Ft/db",
-    "4000 Vákuumtasak 160*400 25Ft/db"
+    "24000 Vákuumtasak PA/PE 70 150*200 12Ft/db",
+    "19000 Vákuumtasak PA/PE 70 150*400 24Ft/db",
+    "30000 Vákuumtasak PA/PE 70 150*500 30Ft/db",
+    "18000 Vákuumtasak PA/PE 70 160*200 13Ft/db",
+    "4000 Vákuumtasak PA/PE 70 160*400 25Ft/db"
 ];
