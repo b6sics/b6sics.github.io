93c3fcea25e51d565cd7c08132994c5d2e72e40d
kissjgabi <kissjgabi@gmail.com>
2022-01-13 15:29:57 +0100

    Update goods.js

--- levAIpack/goods.js
+++ levAIpack/goods.js
@@ -1,10 +1,10 @@
 const goods = [
-    "24000 Vákumtasak PA/PE 70 150*200 12Ft/db",
-    "19000 Vákumtasak PA/PE 70 150*400 24Ft/db",
-    "30000 Vákumtasak PA/PE 70 150*500 30Ft/db",
-    "18000 Vákumtasak PA/PE 70 160*200 13Ft/db",
-    "4000 Vákumtasak PA/PE 70 160*400 25Ft/db",
-    "14000 Vákumtasak PA/PE 90 200*400 39Ft/db",
-    "8000 Vákumtasak PA/PE 90 250*450 55Ft/db",
-    "8000 Vákumtasak PA/PE 90 300*600 87Ft/db"
+    "PA/PE 70,24000,Vákumtasak 150*200,12Ft/db",
+    "PA/PE 70,19000,Vákumtasak 150*400,24Ft/db",
+    "PA/PE 70,30000,Vákumtasak 150*500,30Ft/db",
+    "PA/PE 70,18000,Vákumtasak 160*200,13Ft/db",
+    "PA/PE 70,4000,Vákumtasak 160*400,25Ft/db",
+    "PA/PE 90,14000,Vákumtasak 200*400,39Ft/db",
+    "PA/PE 90,8000,Vákumtasak 250*450,55Ft/db",
+    "PA/PE 90,8000,Vákumtasak 300*600,87Ft/db"
 ];
