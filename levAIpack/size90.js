a2820feec61574c4d9b7e1a4c852bee538dd067d
kissjgabi <kissjgabi@gmail.com>
2022-01-13 13:00:23 +0100

    Update size90.js

--- levAIpack/size90.js
+++ levAIpack/size90.js
@@ -1,5 +1,5 @@
 const size90 = [
-    "14000 Vákuumtasak 200*400 39Ft/db",
-    "8000 Vákuumtasak 250*450 55Ft/db",
-    "8000 Vákuumtasak 300*600 87Ft/db"
+    "14000 Vákuumtasak PA/PE 90 200*400 39Ft/db",
+    "8000 Vákuumtasak PA/PE 90 250*450 55Ft/db",
+    "8000 Vákuumtasak PA/PE 90 300*600 87Ft/db"
 ];
