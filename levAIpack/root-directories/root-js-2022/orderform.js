341bf9852c7d4c92873a99fcf99fa5c43994fe46
kissjgabi <kissjgabi@gmail.com>
2022-01-13 14:03:20 +0100

    Update orderform.js

--- levAIpack/root-directories/root-js-2022/orderform.js
+++ levAIpack/root-directories/root-js-2022/orderform.js
@@ -1,1 +1,29 @@
+let firstOption = '<option value=" ? ">válassz!</option>';
+
 let productGroups = document.getElementById('productGroups');
+
+const productGroupArray = [];
+
+function addProductGroupOption(item, index) {
+    productGroupArray.innerHTML += item.option;
+}
+
+function setProductGroupsOptions() {
+    productGroups.innerHTML = firstOption;
+    productGroupArray.forEach(addProductGroupOption);
+}
+
+let productGroupId = 0;
+
+function addProductGroup(item) {
+    let datas = item.split(" ");
+    element = {
+        id: productGroupId,
+        name: `${datas[2]} ${datas[3]}`,
+    }
+    productGroupId++;
+}
+
+function initProductGroupArray() {
+    goods.forEach(addProductGroup);
+}
