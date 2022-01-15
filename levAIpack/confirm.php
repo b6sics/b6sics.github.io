95d90bfb6ae8d9f3df6220624b8442b6ffea2c91
kissjgabi <kissjgabi@gmail.com>
2022-01-15 16:22:40 +0100

    Create confirm.php

---
+++ levAIpack/confirm.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, 
+                                   initial-scale=1, 
+                                   shrink-to-fit=no" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Megerősítve|Levai Pack</title>
+    <base target="_self">
+
+    <link rel="stylesheet" href="https://b6sics.github.io/levAIpack/root-directories/root-css-2022/style.css?t=123" />
+
+</head>
+
+<body id="start">
+    <a href="index3d2022.html" title="Főoldal">
+        <div class="animation">
+            <div class="cube-mini">
+                <div class="cube_face-mini cube_face-front-mini">Kft.</div>
+                <div class="cube_face-mini cube_face-left-mini">Pack</div>
+                <div class="cube_face-mini cube_face-top-mini">Lévai</div>
+            </div>
+        </div>
+    </a>
+    <header class="right vertical">
+        <h1>
+            Lévai&nbsp;Pack&nbsp;Kft.
+        </h1>
+    </header>
+
+    <main>
+        <header id="orderText">
+            <h2>
+                <i>Megrendelésed&nbsp;feldolgozás alatt</i>
+            </h2>
+        </header>
+
+    </main>
+
+    <footer class="bottom center static">
+        <h4>
+            HU-1113 Budapest Ábel Jenő utca 7.<br />
+            &#9993; info@levaipack.hu | &#9743; 30-7434249
+        </h4>
+    </footer>
+
+<!--?php
+    if($_POST['submitOrder'] != '' || isset($_POST['submitOrder'])) {
+        foreach($_POST as $key => $val) {
+            echo 'Field name : '.$key .', Value : '.$val.'<br>';
+            $data[$key] = $val;
+        }
+    }
+?-->
+
+    <script>
+    </script>
+</body>
+
+</html>
