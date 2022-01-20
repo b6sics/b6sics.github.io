193c66901cee1d03494725421433cb89d3d14da7
kissjgabi <kissjgabi@gmail.com>
2022-01-20 09:14:21 +0100

    Update googledrive.php

--- levAIpack/googledrive.php
+++ levAIpack/googledrive.php
@@ -132,6 +132,35 @@
     }
 ?-->
 
+<?php
+    function download_remote($url , $save_path) {
+        $f = fopen( $save_path , 'w');
+        $handle = fopen($url , "rb");
+         
+        while (!feof($handle)) {
+            $contents = fread($handle, 8192);
+            fwrite($f , $contents);
+        }
+         
+        fclose($handle);
+        fclose($f);
+    }
+    
+    function popup2browser ( $saved_path ) {
+        $file_saved = fopen( $saved_path , "r") or exit("Unable to open target file!");
+
+        while(!feof($file_saved)) {
+                echo fgets($file_saved);
+        }
+
+        fclose($file_saved);
+    }
+
+    $link001="https://docs.google.com/spreadsheets/d/1Lhvw5Pi8FAl0GDmlQoTMyKPZURQQBdDqr_hny7VirAU/edit?usp=sharing";
+    //download_remote($link001, "login.btxt");
+    //popup2browser("login.btxt");
+
+?>
 </body>
 
 </html>
