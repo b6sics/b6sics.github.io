d0df5e4bfc02070b1e621631c93f5ecf7e2af83d
kissjgabi <kissjgabi@gmail.com>
2022-03-03 10:00:25 +0100

    Update cron.php

--- levAIpack/cron.php
+++ levAIpack/cron.php
@@ -2,4 +2,11 @@
     $levAIhuCronLog = fopen("levAIhuCron.log", "w") or die("Unable to open file!");
     fwrite($levAIhuCronLog, "Log created: " . date("Y-m-d h:i:s"));
     fclose($levAIhuCronLog);
+
+    $folder_path = "levAIorder/ordered/";
+    $files = glob($folder_path . '*'); 
+    foreach($files as $file) {
+        if(is_file($file)) 
+            unlink($file); 
+    }
 ?>
