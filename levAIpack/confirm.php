0bd767c016b67947cdc33ae6fb61e4d43ece89c3
kissjgabi <kissjgabi@gmail.com>
2022-03-03 09:35:34 +0100

    Update confirm.php

--- levAIpack/confirm.php
+++ levAIpack/confirm.php
@@ -8,10 +8,10 @@ $method = "aes256";
 $iv_length = openssl_cipher_iv_length($method);
 $iv = openssl_random_pseudo_bytes($iv_length);
 $encrypted_message = openssl_encrypt($message_to_encrypt, $method, $secret_key, 0, $iv);
-echo $encrypted_message;
+echo $encrypted_message, "<br />";
 
 $decrypted_message = openssl_decrypt($encrypted_message, $method, $secret_key, 0, $iv);
-echo $decrypted_message;
+echo $decrypted_message, "<br />";
 ?>
 
 <head>
