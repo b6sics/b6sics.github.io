a63c1181d46919c79a600414f296206c1242b893
kissjgabi <kissjgabi@gmail.com>
2022-05-29 00:05:45 +0200

    Create validation_email.php

---
+++ absoroot/validation_email.php
@@ -0,0 +1,17 @@
+<?php
+
+$home_url = 'Location: https://b6.hu';
+
+$security_mail = 'security@levaipack.hu'; /* 3miLE6lOo1a8ll4 */
+
+function email_is_valid($email_address)
+{
+    $email_address = filter_var($email_address, FILTER_SANITIZE_EMAIL);
+    if (filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+        return $email_address;
+    } else {
+        return $security_mail; 
+    }
+}
+
+?>
