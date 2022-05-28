61e83e9b708d2b48d043d704db32d55154486fb7
kissjgabi <kissjgabi@gmail.com>
2022-05-28 11:27:39 +0200

    Update confirm.php

--- absoroot/confirm.php
+++ absoroot/confirm.php
@@ -4,6 +4,13 @@ if ($_GET['m'] != '' || isset($_GET['m'])) {
         //echo 'Field name : '.$key .', Value : '. base64_decode($val) .'<br>';
         $data[$key] = base64_decode($val);
     }
+} else {
+    header('Location: https://b6.hu');
+}
+
+if ($_GET['d'] != '' || isset($_GET['d'])) {
+} else {
+    header('Location: https://b6.hu');
 }
 
 function salt3()
