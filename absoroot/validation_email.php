9728b1e9d6ca2f8cd7b5398edf9446b76d767a12
kissjgabi <kissjgabi@gmail.com>
2022-06-06 21:51:34 +0200

    Update validation_email.php

--- absoroot/validation_email.php
+++ absoroot/validation_email.php
@@ -6,6 +6,7 @@ $security_mail = 'security@levaipack.hu'; /* 3miLE6lOo1a8ll4 */
 
 function email_is_valid($email_address)
 {
+    GLOBAL $security_mail;
     $email_address = filter_var($email_address, FILTER_SANITIZE_EMAIL);
     if (filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
         return $email_address;
