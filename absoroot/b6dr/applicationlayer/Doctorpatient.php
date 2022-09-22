f146e6798fc25fffd5854023c729d6c4f276bbb5
kissjgabi <b6@sryer.com>
2022-09-22 09:55:42 +0200

    Update Doctorpatient.php

--- absoroot/b6dr/applicationlayer/Doctorpatient.php
+++ absoroot/b6dr/applicationlayer/Doctorpatient.php
@@ -4,7 +4,6 @@
 <head>
 	<title>Doctor Patient</title>
 	<link rel="stylesheet" href="doctorpatient.css">
-
 	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
@@ -14,9 +13,7 @@
 <body>
 	<div class="wrapper">
 		<div class="btn"> <a href="login3.php">
-
 				<button type="button" class="buttonA"> Admin</button>
-
 		</div>
 
 	</div>
