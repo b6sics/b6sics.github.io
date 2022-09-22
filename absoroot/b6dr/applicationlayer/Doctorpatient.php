b27879e0ca97a50107e29fe7c9c23d4cf1d8756b
kissjgabi <b6@sryer.com>
2022-09-22 09:55:07 +0200

    Update Doctorpatient.php

--- absoroot/b6dr/applicationlayer/Doctorpatient.php
+++ absoroot/b6dr/applicationlayer/Doctorpatient.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Doctor Patient</title>
-	<link rel="stylesheet"  href="doctorpatient.css">
+	<link rel="stylesheet" href="doctorpatient.css">
 
 	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
@@ -12,39 +13,40 @@
 
 <body>
 	<div class="wrapper">
-		<div class="btn" > <a href="login3.php">
+		<div class="btn"> <a href="login3.php">
 
-<button type="button" class="buttonA" > Admin</button>
+				<button type="button" class="buttonA"> Admin</button>
 
-</div>
+		</div>
 
-</div>
-
-
-	<div class="wrapper" style="text-decoration: none;">
-	<div class="btn" > <a href="login.php"  >
-<button type="button" class="buttonP">Patient</button>
-</div>
-
-</div>
+	</div>
 
 
 	<div class="wrapper" style="text-decoration: none;">
-	<div class="btn" > <a href="login2.php"  >
-<button type="button" class="buttonD">Doctor</button>
-        </a>
-</div>
+		<div class="btn"> <a href="login.php">
+				<button type="button" class="buttonP">Patient</button>
+		</div>
 
-</div>
-
-	
+	</div>
 
 
- 
-	
+	<div class="wrapper" style="text-decoration: none;">
+		<div class="btn"> <a href="login2.php">
+				<button type="button" class="buttonD">Doctor</button>
+			</a>
+		</div>
+
+	</div>
+
+
+
+
+
+
 
 
 
 
 </body>
+
 </html>
