aeb7b84d7c26ad437087708f0c5ae00cc0cb218c
kissjgabi <b6@sryer.com>
2022-09-24 11:34:46 +0200

    Create cal.php

---
+++ absoroot/cal.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,
+                                   initial-scale=1,
+                                   maximum-scale=1, 
+                                   shrink-to-fit=no" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Abso-root</title>
+
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="shortcut icon" type="image/png" href="favicon.png" />
+
+    <!--link rel="stylesheet" href="https://b6sics.github.io/absoroot/style.css" /-->
+    <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+    <header>
+        <img class="float_right" src="O.png" alt="" />
+        <div class="align_right">
+            <h1>Abso-root</h1>
+            <h5><i> "O" is safety </i></h5>
+        </div>
+    </header>
+    <main class="main_home">
+
+    </main>
+    <footer>
+        &copy; 2020-<?php echo date("Y");?>
+    </footer>
+
+</body>
+<script>
+</script>
+
+</html>
